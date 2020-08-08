e3e75e56e645d4c7d79333cf8e76e4150b7261da
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 18:40:08 +0100

    added import statements to repositories

---
+++ repositories/producer_repository.py
@@ -0,0 +1,5 @@
+from db.run_sql import run_sql
+from models.producer import Producer
+from models.product import Product
+
+# start with basic CRUD, then pseudocode logic to allow filtered views
---
+++ repositories/product_repository.py
@@ -0,0 +1,6 @@
+from db.run_sql import run_sql
+from models.product import Product
+from models.producer import Producer
+import repositories.producer_repository as producer_repository
+
+# start with basic CRUD, then pseudocode logic to allow filtered views
