3b6b495b5ab90c8fb33f371bc98fc8790b2a82e1
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 14:29:48 +0100

    forgot quotes around '__main__' and needed to save a couple of things

--- run_tests.py
+++ run_tests.py
@@ -2,5 +2,5 @@ import unittest
 from tests.product_test import TestProduct
 from tests.producer_test import TestProducer
 
-if __name__ == __main__:
+if __name__ == '__main__':
     unittest.main()
---
+++ tests/producer_test.py
@@ -0,0 +1,2 @@
+import unittest
+from models.producer import Producer
