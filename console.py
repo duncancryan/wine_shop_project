5af1cddf8c61462e06553b54d378242e682a986c
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 18:55:59 +0100

    added import statements to console.py and imported pdb so that I can run tests onmy CRUD functions

---
+++ console.py
@@ -0,0 +1,5 @@
+import pdb
+from models.product import Product
+from models.producer import Producer
+import repositories.product_repository as product_repository
+import repositories.producer_repository as producer_repository
