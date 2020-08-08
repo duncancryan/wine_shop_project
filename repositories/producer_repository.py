93b796b1b17c4a3273fcf092f910c5603a6711ed
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 18:53:24 +0100

    wrote save function in producer repository

--- repositories/producer_repository.py
+++ repositories/producer_repository.py
@@ -3,3 +3,12 @@ from models.producer import Producer
 from models.product import Product
 
 # start with basic CRUD, then pseudocode logic to allow filtered views
+
+def save(producer):
+    sql = "INSERT INTO producers (name, country, contact_number, contact_email) VALUES (%s, %s, %s, %s) RETURNING *"
+    values = [producer.name, producer.country, producer.contact_number, producer.contact_email]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    producer.id = id
+    return producer
+    
