69c297c6f2ff5d692bc0866f4d75426b565ea0e5
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 17:33:18 +0100

    products table created

--- db/wine_shop.sql
+++ db/wine_shop.sql
@@ -7,4 +7,15 @@ CREATE TABLE producers (
     country VARCHAR(255),
     contact_number VARCHAR(255),
     contact_email VARCHAR(255)
-)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    cost DECIMAL,
+    price DECIMAL,
+    case_price DECIMAL,
+    stock INTEGER,
+    producer_id INT REFERENCES producers(id) ON DELETE CASCADE
+);
