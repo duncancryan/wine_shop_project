8870aa51c5f918ac1788f62714c2849ac3e5b175
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 14:26:49 +0100

    touched test files in test repository, imported unittest and relevant modules into test files and runner

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from tests.product_test import TestProduct
+from tests.producer_test import TestProducer
+
+if __name__ == __main__:
+    unittest.main()
---
+++ tests/product_test.py
@@ -0,0 +1,2 @@
+import unittest
+from models.product import Product
