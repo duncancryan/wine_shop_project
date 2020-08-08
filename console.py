7a4147d20d090199d7cface1173dac17a2ba2529
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 18:59:23 +0100

    pdb trace set at the bottom of the file to track any changes made in the debugger

--- console.py
+++ console.py
@@ -3,3 +3,6 @@ from models.product import Product
 from models.producer import Producer
 import repositories.product_repository as product_repository
 import repositories.producer_repository as producer_repository
+
+
+pdb.set_trace()
