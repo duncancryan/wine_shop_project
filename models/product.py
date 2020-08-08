072752c1e0cc4420d9c71188ad611c83e5077f34
Duncan Ryan <duncancryan1@gmail.com>
2020-08-08 16:37:25 +0100

    setup for product class tests written

---
+++ models/product.py
@@ -0,0 +1,3 @@
+class Product:
+    def __init__(self, name, type, producer, cost, price, case_price, stock, id = None):
+        pass
--- tests/product_test.py
+++ tests/product_test.py
@@ -1,2 +1,8 @@
 import unittest
 from models.product import Product
+from models.producer import Producer
+
+class TestProduct(unittest.TestCase):
+    def setUp(self):
+        self.producer = Producer("Black Friars Distillery", "UK", "01752 665 292", "info@plymouthdistillery.com")
+        self.product = Product("Plymouth Gin", "Gin", self.producer, 10.50, 22.00, 115.00, 43)
