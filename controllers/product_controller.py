793459bb13bef03f26580316ece5c5c2fa63d866
Duncan Ryan <duncancryan1@gmail.com>
2020-08-09 18:32:59 +0100

    show route for products working

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -9,4 +9,9 @@ products_blueprint = Blueprint("products", __name__)
 @products_blueprint.route("/products")
 def products():
     products = product_repository.select_all()
-    return render_template('products/index.html', products=products)
+    return render_template('/products/index.html', products=products)
+
+@products_blueprint.route("/products/<id>")
+def show_product(id):
+    product = product_repository.select(id)
+    return render_template("/products/show.html", product=product)
