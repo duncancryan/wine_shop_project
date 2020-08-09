638f6ae83e14e5a1b4937cc61acd410039080711
Duncan Ryan <duncancryan1@gmail.com>
2020-08-09 10:48:17 +0100

    save CRUD function written for product

--- repositories/product_repository.py
+++ repositories/product_repository.py
@@ -4,3 +4,13 @@ from models.producer import Producer
 import repositories.producer_repository as producer_repository
 
 # start with basic CRUD, then pseudocode logic to allow filtered views
+
+def save(product):
+    sql "INSERT INTO products (name, type, cost, price, case_price, stock, producer_id) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [product.name, product.type, product.cost, product.price, product.case_price, product.stock, product.producer.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    product.id = id
+    return product
+
+    
